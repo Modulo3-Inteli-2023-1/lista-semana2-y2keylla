bc05341df99b56921320751485f6a0094a2fb100
Keylla Oliveira <112659414+y2keylla@users.noreply.github.com>
2023-08-18 20:28:36 -0300

    Update exercicio2.py

--- exercicio2.py
+++ exercicio2.py
@@ -5,7 +5,23 @@
 
 
 # Crie a função que será avaliada no exercício aqui
+def is_anagram(string1, string2):
+    lista1 = list(string1.lower())
+    lista2 = list(string2.lower())
 
+    lista1.sort()
+    lista2.sort()
+
+    return lista1 == lista2
+
+# Exemplo de uso:
+string1 = "Roma"
+string2 = "amor"
+
+if is_anagram(string1, string2):
+    print(True)
+else:
+    print(False)
 
 
 
