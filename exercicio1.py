988de4295109c00eb37e6bf5dc9ee1c902f7721b
Keylla Oliveira <112659414+y2keylla@users.noreply.github.com>
2023-08-18 20:28:19 -0300

    Update exercicio1.py

--- exercicio1.py
+++ exercicio1.py
@@ -5,10 +5,17 @@
 
 
 # Crie a função que será avaliada no exercício aqui
+def conta_palavras_unicas(str):
+    palavra = str.split()
+    palavras_unicas = set()
 
-
-
-
+    for p in palavra:
+        if p not in palavras_unicas:
+            palavras_unicas.add(p)
+    return len(palavras_unicas)
+# Exemplo de uso:
+frase = "Oi tudo bem bem"
+print(conta_palavras_unicas(frase))
 
 
 
